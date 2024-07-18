2bbcb33258f14d517a16d3742828c564e4701680
Kernell69 <zurakraimer@gmail.com>
2024-07-18 19:47:18 -0300

    agregando layouts

--- harmonyhub/src/App.jsx
+++ harmonyhub/src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/layouts/footer';
+import Header from './components/layouts/Header';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import HomePage from './pages/HomePages';
@@ -9,12 +11,14 @@ function App() {
   
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/'element = {<HomePage/>} />
         <Route path='/login'element = {<LoginPage/>} />
         <Route path='/register'element = {<RegisterPage/>} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </Router>
   
   )
---
+++ harmonyhub/src/components/layouts/footer.jsx
@@ -0,0 +1,9 @@
+
+const Footer = () => {
+    return (
+      <p className="text-orange-400">Footer</p>
+    )
+  }
+  
+  export default Footer
+  
